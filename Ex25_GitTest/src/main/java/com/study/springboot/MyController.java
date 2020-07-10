efbf834cd719b24b1b32b75e098e7785bd3037d9
user <user@i7C-56>
2020-07-10 10:08:18 +0900

    초기버전 회원가입 추가 로그인 추가 aaa.jsp수정

---
+++ Ex25_GitTest/src/main/java/com/study/springboot/MyController.java
@@ -0,0 +1,28 @@
+package com.study.springboot;
+
+public class MyController {
+
+		//Git Test 
+		//Git 사용하는 장점
+		//1. 소스 안전 관리
+		//2. 히스토리 관리가 가능하다 - 수정/추가/삭제/변경 기록이 다 남음.
+		//3. 팀 작업 용이
+	
+		
+		//프로젝트 소스 폴더
+		//	|		    ▲
+		//	▼  commit   | fetch
+		//로컬 PC 저장소
+		//	|		 ▲
+		//	▼  push  | pull
+		//원격 저장소(Github 서버)
+		
+		//add an existing local git repository 로컬 저장소선
+		//Clone : 복제 ( 서버저장소를 로컬에 복제함.)
+			//Master : 최상단 소스 노드
+			//Branch : 가지치기 master ------------------->
+			//							|			   ^ merge
+			//							---> branch -- |
+	
+	
+}
